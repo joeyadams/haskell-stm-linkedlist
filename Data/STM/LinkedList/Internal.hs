5d74150ee01b4ab9b4c274f89e2f558084b393bd
Joey Adams <joeyadams3.14159@gmail.com>
2011-10-19 18:44:42 -0400

    Whoops again: nodePrev and nodeNext need to be different pointers; going back to LinkedList (Node a)

--- Data/STM/LinkedList/Internal.hs
+++ Data/STM/LinkedList/Internal.hs
@@ -9,33 +9,44 @@ import Control.Concurrent.STM
 import System.IO (fixIO)
 
 -- | Circular doubly linked list.
-newtype LinkedList a = LinkedList (NodePtr a)
+newtype LinkedList a = LinkedList (Node a)
 
 data Node a
     = Node
         { nodePrev  :: NodePtr a
         , nodeNext  :: NodePtr a
-        , nodeValue :: a
-            -- ^ Warning: the value of a 'LinkedList' anchor is undefined.
+        , nodeValue :: Maybe a
+            -- ^ 'Nothing' if this is the list head.
         }
 
 type NodePtr a = TVar (Node a)
 
-emptyAnchor :: NodePtr a -> Node a
-emptyAnchor ptr =
-    Node { nodePrev  = ptr
-         , nodeNext  = ptr
-         , nodeValue = error "nodeValue: undefined (list anchor)"
-         }
-
 empty :: STM (LinkedList a)
 empty = do
-    anchor <- newTVar undefined
-    writeTVar anchor $ emptyAnchor anchor
-    return $ LinkedList anchor
+    prev_ptr <- newTVar undefined
+    next_ptr <- newTVar undefined
+    let node = Node prev_ptr next_ptr Nothing
+    writeTVar prev_ptr node
+    writeTVar next_ptr node
+    return $ LinkedList node
 
 -- | IO version of 'empty'.
 emptyIO :: IO (LinkedList a)
 emptyIO = do
-    anchor <- fixIO (newTVarIO . emptyAnchor)
-    return $ LinkedList anchor
+    node <- fixIO $ \node -> do
+        prev_ptr <- newTVarIO node
+        next_ptr <- newTVarIO node
+        return (Node prev_ptr next_ptr Nothing)
+    return $ LinkedList node
+
+prepend :: a -> LinkedList a -> STM (Node a)
+prepend = undefined
+
+append :: a -> LinkedList a -> STM (Node a)
+append = undefined
+
+remove :: Node a -> STM ()
+remove = undefined
+
+toList :: LinkedList a -> STM [a]
+toList = undefined
